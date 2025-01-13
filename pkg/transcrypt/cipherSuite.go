cdd19539786fe7e7e716cef7c078c24481a9c895
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-01-13 14:53:49 +0100

    cipherSuite.go:
    - Add documentation
    - Add tests

--- pkg/transcrypt/cipherSuite.go
+++ pkg/transcrypt/cipherSuite.go
@@ -1,12 +1,16 @@
 package transcrypt
 
-type CipherSuite byte
-
 const (
 	AES_256_GCM CipherSuite = iota
 	CHACHA20_POLY1305
 )
 
+// CipherSuite defines which cipher suites can be used for transcryption of data.
+// It is based on the types available in github.com/minio/sio .
+type CipherSuite byte
+
+// GetCipherSuite converts a string into its respective CipherSuite.
+// It returns CHACHA20_POLY1305 by default if the string cannot be converted.
 func GetCipherSuite(s string) CipherSuite {
 	switch s {
 	case "AES_256_GCM":
